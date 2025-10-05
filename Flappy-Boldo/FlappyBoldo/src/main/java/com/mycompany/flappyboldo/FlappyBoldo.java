7e96ed3e6a2aeaf89eb5cebf59ff96a446f2ee2f
Arenas Tiago Rodrigo <129696119+ArenasTI@users.noreply.github.com>
2025-10-05 19:11:13 -0300

    Se agrega lo avanzado hasta el momento

---
+++ Flappy-Boldo/FlappyBoldo/src/main/java/com/mycompany/flappyboldo/FlappyBoldo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flappyboldo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Flappy-Boldo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
